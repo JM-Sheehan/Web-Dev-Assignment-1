e8619639d225e563a76c8a8bc2849c15d54d3238
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-08-24 19:57:30 +0100

    Update tmdb-api.js

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -6,14 +6,6 @@ export const getMovies = () => {
       .then(json => json.results);
   };
 
-  export const getMovieCredits = id => {
-    return fetch(
-      `https://api.themoviedb.org/3/discover/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };
-
 
   export const getPopularPeople = () => {
     return fetch(
@@ -68,19 +60,4 @@ export const getMovies = () => {
     else{
       return "male";
     }
-  };
-
-  export const newGuestSession = () =>{
-    return fetch(
-      `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    ).then(res => res.json());
-  };
-
-  export const addRating = (movie_id, rating, guest_id) =>{
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${movie_id}/
-      ${rating}?api_key=${process.env.REACT_APP_TMDB_KEY}
-      &guest_session_id=${guest_id}`
-    )
-    .then(res => res.json());
   };
